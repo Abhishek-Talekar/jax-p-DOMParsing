3b474880afa51b9fde1934c308bb15e8b64a1527
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-02-14 23:40:15 +0530

    DocumentBuilderFactory is used to get the Vendor Implementation class

---
+++ DOM/src/com/dom/test/DOMParser.java
@@ -0,0 +1,23 @@
+package com.dom.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+public class DOMParser {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = builderFactory.newDocumentBuilder();
+		Document document = builder.parse(new File("C:\\Users\\abhi\\git\\jax-p-DOMParsing\\DOM\\Resources\\po.xml"));
+		String node = document.getFirstChild().getNodeName();
+		//String node =document.getFirstChild().getLocalName();
+		System.out.println(node);
+	}
+}
